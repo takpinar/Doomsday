f848625c07ee1ea832cddbbe37328764489a02ec
takpinar <theo.akpinar@gmail.com>
2022-11-25 01:29:57 -0500

    fix num-qs

--- dd_quiz.py
+++ dd_quiz.py
@@ -4,7 +4,8 @@ import random
 from termcolor import colored
 
 faker = faker.Faker()
-num_qs = random.randrange(5,10)
+# num_qs = random.randrange(5,10)
+num_qs = 7
 i=0
 sum_time = 0
 score = 0
@@ -31,12 +32,14 @@ while i<num_qs:
         print(colored('X','red'))
         # time.sleep(1)
         
-tot_time = round(time.time() - begin,2)
+tot_time = time.time() - begin
+mins = round(tot_time//60,0)
+secs = round(tot_time%60,0)
 perc = round(score/num_qs,2)
 avg_time = round(sum_time/num_qs,2)
 
-print('\n---------------------------------------')
+print('---------------------------------------')
 print("Accuracy:     {} %".format(perc))
+print("Total Time:   {} min. {} sec.".format(mins, secs))
 print("Average Time: {} sec.".format(avg_time))
-print("Total Time:   {} sec.".format(tot_time))
 print('---------------------------------------')
