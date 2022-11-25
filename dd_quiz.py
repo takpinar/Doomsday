9aad84fc8de5d0b6340343864028be6b293a85cd
takpinar <theo.akpinar@gmail.com>
2022-11-25 16:58:26 -0500

    added hi-score functionality, and colored score outputs

--- dd_quiz.py
+++ dd_quiz.py
@@ -4,11 +4,16 @@ import random
 from termcolor import colored
 
 faker = faker.Faker()
-# num_qs = random.randrange(5,10)
-num_qs = 7
-i=0
+num_qs   = 5
+i        = 0
 sum_time = 0
-score = 0
+score    = 0
+colors   = ['red','pink','orange','yellow','blue','green']
+
+file = open("hi_score.txt","r")
+hisc = file.read()
+file.close()
+
 begin = time.time()
 
 while i<num_qs:
@@ -30,16 +35,28 @@ while i<num_qs:
         # time.sleep(1)
     else:
         print(colored('X','red'))
-        # time.sleep(1)    
+        # time.sleep(1)     
     
+perc = round(score/num_qs,2)*100
 tot_time = time.time() - begin
+
+best = False
+if (perc == 1.0 and float(hisc[8:-1]) > tot_time) or hisc == '':
+    print('BEST')
+    file = open("hi_score.txt","w")
+    file.write("hiscore=" + str(round(tot_time,2)))
+    file.close()
+    best = True
+
 mins = round(tot_time//60,0)
 secs = round(tot_time%60,0)
-perc = round(score/num_qs,2)
 avg_time = round(sum_time/num_qs,2)
 
 print('---------------------------------------')
-print("Accuracy:     {} %".format(perc))
-print("Total Time:   {} min. {} sec.".format(mins, secs))
+print(colored("Accuracy:     {} %".format(perc),colors[score]))
+if best:
+    print(colored("New Best Time:   {} min. {} sec.".format(mins, secs),'green'))
+else:
+    print("Total Time:   {} min. {} sec.".format(mins, secs))
 print("Average Time: {} sec.".format(avg_time))
 print('---------------------------------------')
