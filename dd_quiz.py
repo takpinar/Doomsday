3d2a36e7b8074f56a372261ad2f3c6ca63339bcc
takpinar <theo.akpinar@gmail.com>
2022-11-25 00:24:31 -0500

    Added average time, accuracy, and printing frills

--- dd_quiz.py
+++ dd_quiz.py
@@ -1,20 +1,38 @@
 import faker
 import time
+import random
+from termcolor import colored
 
 faker = faker.Faker()
-
+num_qs = random.randrange(5,10)
 i=0
-while i<5:
+sum_time = 0
+score = 0
+
+while i<num_qs:
     i+=1
     rand_date = faker.date_between('-400y','+200y')
     date_str = rand_date.strftime('%B %d, %Y')
     weekday_str = rand_date.strftime('%A')
+    question = date_str + ' ?\n'
 
-    ans = input(date_str)
+    start = time.time()
+    print('*-.-*-.-*-.-*-.-*-.-*-.-*-.-*-.-*')
+    ans = input(question)
+    dur = time.time() - start
+    sum_time += dur
+
     if ans.lower()==weekday_str.lower():
-        print('\n\N{check mark}\N{check mark}\N{check mark}\N{check mark}\N{check mark}\N{check mark}\N{check mark}\N{check mark} Correct! \N{check mark}\N{check mark}\N{check mark}\N{check mark}\N{check mark}\N{check mark}\N{check mark}\N{check mark}')
+        score += 1
+        print(colored('\N{check mark}','green'))
         time.sleep(1)
     else:
-        print('\nXxXxXxXx Inorrect xXxXxXxX')
+        print(colored('X','red'))
         time.sleep(1)
 
+perc = round(score/num_qs,2)
+avg_time = round(sum_time/num_qs,2)
+print('\n---------------------------------------')
+print("Accuracy: ",perc, '%')
+print("Average Time: ", avg_time,'sec.')
+print('---------------------------------------')
