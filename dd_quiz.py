613bed53d6d3d411d7d07f4bb1db787f7c848c50
takpinar <theo.akpinar@gmail.com>
2022-11-30 02:23:20 -0500

    misc

--- dd_quiz.py
+++ dd_quiz.py
@@ -8,7 +8,7 @@ num_qs   = 5
 i        = 0
 sum_time = 0
 score    = 0
-colors   = ['red','pink','orange','yellow','blue','green']
+colors   = ['red','yellow','cyan','green']
 
 hi_score_path = "/home/takpinar/desktop/Doomsday/hi_score.txt"
 file = open(hi_score_path,"r")
@@ -31,8 +31,11 @@ while i<num_qs:
     ans = input(question)
     dur = time.time() - start
     sum_time += dur
-
-    if ans.lower()==weekday_str.lower():
+    
+    if ans == 'q':
+        print('quit')
+        quit()
+    elif ans.lower()==weekday_str.lower():
         score += 1
         print(colored('\N{check mark}','green'))
         time.sleep(.1)
@@ -43,11 +46,13 @@ while i<num_qs:
 perc = round(score/num_qs,2)*100
 best = False
 
-if (perc == 1.0 and hisc > sum_time) or hisc == '':
+if (perc == 100.0 and hisc > sum_time) or hisc == '':
     print('BEST')
-    file = open("hi_score.txt","w")
+    file = open(hi_score_path,"w")
     file.write("hiscore=" + str(round(sum_time,2)))
     file.close()
+    himins = round(sum_time//60,1)
+    hisecs = round(sum_time%60,1)
     best = True
 
 mins = round(sum_time//60,0)
@@ -55,7 +60,7 @@ secs = round(sum_time%60,0)
 avg_time = round(sum_time/num_qs,2)
 
 print('---------------------------------------')
-print(colored("Accuracy:     {} %".format(perc),colors[score]))
+print(colored("Accuracy:     {} %".format(perc),colors[int((score/num_qs)//0.3)]))
 if best:
     print(colored("New Best Time:   {} min. {} sec.".format(mins, secs),'green'))
 else:
